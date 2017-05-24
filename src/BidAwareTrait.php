e39acdd86e9eab6889ea74a84dd6434a5456da45
Matthias Bianchi <matthias.bianchi.57@gmail.com>
2017-05-24 10:55:32 +0200

    Create the package

---
+++ src/BidAwareTrait.php
@@ -0,0 +1,32 @@
+<?php
+namespace Fei\Service\Bid\Package;
+
+use Fei\Service\Bid\Client\Bidder;
+
+/**
+ * Class BidAwareTrait
+ * @package Fei\Service\Bid\Package
+ */
+trait BidAwareTrait
+{
+    /** @var  Bidder */
+    protected $bidder;
+
+    /**
+     * @return Bidder
+     */
+    public function getBidder(): Bidder
+    {
+        return $this->bidder;
+    }
+
+    /**
+     * @param Bidder $bidder
+     * @return BidAwareTrait
+     */
+    public function setBidder(Bidder $bidder)
+    {
+        $this->bidder = $bidder;
+        return $this;
+    }
+}
---
+++ src/BidPackage.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Fei\Service\Bid\Package;
+
+use Fei\ApiClient\AbstractApiClient;
+use Fei\ApiClient\Transport\BasicTransport;
+use Fei\Service\Bid\Client\Bidder;
+use Fei\Service\Bid\Package\Config\BidBaseUrl;
+use ObjectivePHP\Application\ApplicationInterface;
+use ObjectivePHP\ServicesFactory\ServiceReference;
+
+/**
+ * Class BidPackage
+ * @package Fei\Service\Bid\Package
+ */
+class BidPackage
+{
+    const DEFAULT_IDENTIFIER = 'bid.client';
+
+    /** @var string */
+    protected $identifier;
+
+    /**
+     * BidPackage constructor.
+     * @param string $serviceIdentifier
+     */
+    public function __construct(string $serviceIdentifier = self::DEFAULT_IDENTIFIER)
+    {
+        $this->identifier = $serviceIdentifier;
+    }
+
+    /**
+     * @param ApplicationInterface $app
+     * @throws \Exception
+     */
+    public function __invoke(ApplicationInterface $app)
+    {
+        $config = $app->getConfig();
+
+        if (!$config->has(BidBaseUrl::class)) {
+            throw new \Exception('Parameter Base URL for Bid Package does not exist');
+        }
+
+        $baseUrl = $config->get(BidBaseUrl::class);
+
+        $app->getServicesFactory()->registerService([
+            'id' => 'transport.basic',
+            'class' => BasicTransport::class,
+            'params' => [$baseUrl],
+        ]);
+
+        $setters = [
+            'setTransport' => [new ServiceReference('transport.basic')],
+        ];
+
+        $app->getServicesFactory()->registerService(
+            [
+                'id' => $this->identifier,
+                'class' => Bidder::class,
+                'params' => [[AbstractApiClient::OPTION_BASEURL => $baseUrl]],
+                'setters' => $setters
+            ]
+        );
+    }
+}
